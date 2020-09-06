f83aeb9be80064d1f95948ca54def46c07b8bf36
yp.pan <pan.yiping.fi@gmail.com>
2020-09-06 16:20:17 +0800

    Add cfront.rs and def.rs

---
+++ src/cfront.rs
@@ -0,0 +1,3 @@
+// C language front-end 
+
+fn is_whitespace
--- src/defs.rs
+++ src/defs.rs
@@ -1,23 +1,34 @@
 // definitions 
 
 // limitations
-static MAX_TOKEN_LEN:i8 = 256;
-static MAX_ID_LEN:i8 = 64;
-static MAX_LINE_LEN:i8 = 256;
-static MAX_VAR_LEN:i8 = 64;
-static MAX_TYPE_LEN:i8 = 64;
-static MAC_PARAMS:i8 = 8;
-static MAX_LOCALS:i8 = 48;
-static MAX_FIELD:i8 = 32;
-static MAX_FUNCT:i16 = 1024;
-static MAX_BLOCKS:i16 = 65535;
-static MAX_TYPES:i8 = 64;
-static MAX_IR_INSTR:i16 = 65535;
-static MAX_SOURCE:i32 = 131072;
-static MAX_CODE:i32 = 131072;
-static MAX_DATA:i32 = 131072;
+static MAX_TOKEN_LEN:u16 = 256;
+static MAX_ID_LEN:u8 = 64;
+static MAX_LINE_LEN:u16 = 256;
+static MAX_VAR_LEN:u8 = 64;
+static MAX_TYPE_LEN:u16 = 64;
+static MAC_PARAMS:u8 = 8;
+static MAX_LOCALS:u8 = 48;
+static MAX_FIELD:u8 = 32;
+static MAX_FUNCT:u32 = 1024;
+static MAX_BLOCKS:u32 = 65536;
+static MAX_TYPES:u8 = 64;
+static MAX_IR_INSTR:u32 = 65536;
+static MAX_SOURCE:u32 = 131072;
+static MAX_CODE:u32 = 131072;
+static MAX_DATA:u32 = 131072;
+static MAX_SYMTAB:u32 = 65536;
+static MAX_STRTAB:u32 = 65536;
+static MAX_HEADER:u16 = 1024;
+static MAX_SECTION:u16 = 1024;
+static MAX_ALIASES:u16 = 1024;
+static MAX_CONSTANTS:u16 = 1024;
+static MAX_CASES:u8 = 128;
+static MAX_NESTING:u8 = 128;
 
+static ELF_START:usize = 0x10000;
+static PTR_SIZE: u8 = 4;
 
+// builtin types
 enum BaseTypeT {
     TypeVoid = 0,
     TypeInt,
@@ -25,6 +36,7 @@ enum BaseTypeT {
     TypeCtruct,
 }
 
+// IR opcode
 enum OpcodeT {
     // generic: intermediate use in front-end. No code generation
     OpGeneric,
@@ -79,3 +91,75 @@ enum OpcodeT {
     OpStart,
 }
 
+// IR instruction
+pub struct IRInstrT {
+    op:OpcodeT,
+    op_len: usize,
+    ir_index: usize,
+    code_offset: usize,
+    param_no: usize,
+    int_param1: usize,
+    int_param2: usize,
+    str_param1: str,         // not sure!
+}
+
+// variable definition 
+pub struct VarT {
+    type_name: String,
+    var_name: String,
+    is_ptr: bool,
+    array_size: usize,
+    offset:usize,     // offset from stack or frame
+}
+
+// function definition
+pub struct FuncT {
+    return_def: VarT,
+    param_defs: VarT,
+    num_params: usize,
+    entry_point: usize,  // IR index
+    exit_point: usize,   // IR index
+    params_size: usize,   
+}
+
+// block definition
+pub struct BlockT<'a> {
+   locals: VarT,
+   next_local: usize,
+   parent: &'a mut BlockT<'a>,
+   func_t: &'a mut FuncT,
+   local_size: usize,
+   index: usize,
+}
+
+// type definition
+pub struct TypeT {
+    type_name: String<>,
+    base_type: BaseTypeT,
+    size: usize,
+    fields: VarT,
+    num_fields: usize,
+}
+
+// lvalue details
+pub struct LvalueT {
+    size: usize,
+    is_ptr: bool,
+    r#type: TypeT,      // escape reserved keywords to use them as identifiers
+}
+
+// alias for #defines
+pub struct AliasT {
+    alias: String<>,
+    value: String<>,
+}
+
+// constants for enum
+pub struct ConstantT {
+    alias: String<>,
+    value: usize,
+}
+
+fn main(){
+
+}
